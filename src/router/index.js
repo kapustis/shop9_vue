20346d6355c7e35c2ac1cc4f5bf4304043e734a1
Gennady <kapustis73@gmail.com>
2023-01-13 16:09:36 +0200

    init

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'some',
+      component: () => import('../views/Some.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/Some.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Some component
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Some"
+}
+</script>
+
+<style scoped>
+
+</style>
